4bcfb93059586231ae9000dfb5af935aac199f2e
Milen Denev <mspc6d25@hotmail.com>
2024-12-25 18:31:11 +0200

    Update documentation and chunk size to 1MB.

--- rasterizeddb_core/src/lib.rs
+++ rasterizeddb_core/src/lib.rs
@@ -3,7 +3,7 @@ use moka::sync::Cache;
 use once_cell::sync::Lazy;
 
 pub(crate) const HEADER_SIZE: u16 = 30;
-pub(crate) const CHUNK_SIZE: u32 = 250_000;
+pub(crate) const CHUNK_SIZE: u32 = 1_000_000;
 
 pub(crate) const EMPTY_BUFFER: [u8; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
 
@@ -45,22 +45,16 @@ pub(crate) static POSITIONS_CACHE: Lazy<Cache<u64, Vec<(u64, u32)>, RandomState>
 /// columns_buffer.append(&mut c2.into_vec().unwrap());
 /// columns_buffer.append(&mut c3.into_vec().unwrap());
 /// 
-/// let table_clone = TABLE.clone();
-/// let mut mutable_table = table_clone.write().await;
-/// 
-/// // Can be combined with tokio::Spawn as well
-/// mutable_table.insert_row(&mut InsertRow {
-///     columns_data: columns_buffer
-/// }).await.unwrap();
+/// table.insert_row(insert_row).await;
 /// ```
 /// #### Build in-memory file indexes
 /// ```rust
-/// mutable_table.rebuild_in_memory_indexes();
+/// table.rebuild_in_memory_indexes();
 /// ```
 /// 
 /// #### Retrieve a row
 /// ```rust
-/// let row_by_id = mutable_table.first_or_default_by_id(10).unwrap().unwrap();
+/// let row_by_id = table.first_or_default_by_id(3)?.unwrap();
 /// 
 /// // Read columns
 /// for column in Column::from_buffer(&row2.columns_data).unwrap().iter() {
@@ -68,7 +62,7 @@ pub(crate) static POSITIONS_CACHE: Lazy<Cache<u64, Vec<(u64, u32)>, RandomState>
 /// }
 /// 
 /// // Column index, value that must be equal
-/// let row_by_column_value = mutable_table.first_or_default_by_column(2, "This is awesome").unwrap().unwrap();
+/// let row_by_column_value = table.first_or_default_by_column(2, "This is awesome").unwrap().unwrap();
 /// 
 /// //Rasterized Query Language (Alpha)
 /// let query_evaluation = parse_rql(&format!(r#"
@@ -79,13 +73,13 @@ pub(crate) static POSITIONS_CACHE: Lazy<Cache<u64, Vec<(u64, u32)>, RandomState>
 /// "#)).unwrap();
 /// 
 /// // Uses cache: If the same query is repeated, the time to retrieve a row should be in the single-digit range.
-/// let row_by_query = mutable_table.first_or_default_by_query(query_evaluation).await.unwrap().unwrap();
+/// let row_by_query = table.first_or_default_by_query(query_evaluation).await.unwrap().unwrap();
 /// ```
 /// 
 /// #### Delete a row
 /// ```rust
 /// // Invalidates cache automatically
-/// mutable_table.delete_row_by_id(10).unwrap();
+/// table.delete_row_by_id(10).unwrap();
 /// ```
 pub mod core;
 
