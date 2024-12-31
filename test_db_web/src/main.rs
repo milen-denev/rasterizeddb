2b4769a5d14ad7622f15d7712c9e1f17346dda94
Milen Denev <mspc6d25@hotmail.com>
2025-01-01 00:49:23 +0200

    Added more testing.

--- test_db_web/src/main.rs
+++ test_db_web/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use once_cell::sync::Lazy;
 use rand::rngs::ThreadRng;
 use rand::Rng;
@@ -8,8 +10,10 @@ use rasterizeddb_core::core::{
     table::Table
 };
 use axum::{response::Response, routing::get};
-use axum::Router;
+use axum::{Extension, Router};
 use rasterizeddb_core::rql::parser::parse_rql;
+use tokio::io::{AsyncReadExt, AsyncSeekExt};
+use tokio::sync::RwLock;
 
 // static mut TABLE: Lazy<Table<LocalStorageProvider>> = Lazy::new(|| {
     
@@ -34,9 +38,17 @@ static mut RANDOM: Lazy<ThreadRng> = Lazy::new(|| {
 
 #[tokio::main]
 async fn main() {
+    let file = Arc::new(RwLock::new(
+        tokio::fs::File::open("C:\\Users\\mspc6\\OneDrive\\Professional\\Desktop\\column_data.png")
+        .await
+        .unwrap()
+    ));
+
     let app = Router::new()
         .route("/", get(index))
-        .route("/get", get(query));
+        .route("/get", get(query))
+        .route("/test", get(test_reading))
+        .layer(Extension(file));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:80")
         .await
@@ -47,12 +59,12 @@ async fn main() {
 
 #[allow(static_mut_refs)]
 async fn index() -> Response {
-    let x = unsafe { RANDOM.gen_range(-100_000..100_000) } as f64;
-    let y = unsafe { RANDOM.gen_range(-100_000..100_000) } as f64;
-    let z = unsafe { RANDOM.gen_range(-100_000..100_000) } as f64;
+    let x = unsafe { RANDOM.gen_range(-10..10 as i32) } as i32;
+    let y = unsafe { RANDOM.gen_range(-10..10 as i32) } as i32;
+    let z = unsafe { RANDOM.gen_range(-10..10 as i32) } as i32;
 
-    let mut c1_update = Column::new((x + y) as u64).unwrap();
-    let mut c2_update = Column::new((x / y) * z).unwrap();
+    let mut c1_update = Column::new(x + y).unwrap();
+    let mut c2_update = Column::new((((x as f64) / (y as f64) as f64) * z as f64) as f64).unwrap();
     //let large_string = "A".repeat(1024);
     //let mut c3_update = Column::new(&large_string).unwrap();
 
@@ -79,8 +91,8 @@ async fn index() -> Response {
 
 #[allow(static_mut_refs)]
 async fn query() -> Response {
-    let x = unsafe { RANDOM.gen_range(-100_000..100_000) } as f64;
-    let y = unsafe { RANDOM.gen_range(-100_000..100_000) } as f64;
+    //let x = unsafe { RANDOM.gen_range(-10..10 as i32) } as i32;
+    //let y = unsafe { RANDOM.gen_range(-10..10 as i32) } as i32;
     //let z = unsafe { RANDOM.gen_range(-100_000..100_000) } as f64;
 
     //OR COL(1) = {x} / {y} * {z}
@@ -88,13 +100,29 @@ async fn query() -> Response {
     let query_evaluation = parse_rql(&format!(r#"
             BEGIN
             SELECT FROM NAME_DOESNT_MATTER_FOR_NOW
-            WHERE COL(0) = {x} + {y}
+            WHERE COL(0) > 0
             END
         "#)).unwrap();
 
     let table = unsafe { TABLE.force_mut().await };
 
-    _ = table.first_or_default_by_query(query_evaluation).await;
+    let result = table.first_or_default_by_query(query_evaluation).await.unwrap();
 
-    Response::new("".into())
+    if result.is_some() {
+        Response::builder().status(200).body("".into()).unwrap()
+    } else {
+        Response::builder().status(404).body("".into()).unwrap()
+    }
+}
+
+#[allow(static_mut_refs)]
+async fn test_reading(reader: Extension<Arc<RwLock<tokio::fs::File>>>) -> Response {
+    let reader_clone = reader.clone();
+    let mut write_reader = reader_clone.write().await;
+
+    _ = write_reader.seek(std::io::SeekFrom::Start(6)).await;
+    let mut vec = vec![0; 8];
+    _ = write_reader.read(&mut vec).await;
+
+    Response::new(format!("{:?}", vec).into())
 }
