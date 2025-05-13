8a715b6d2655b237f035d0633291dbfc63e80057
Milen Denev <mspc6d25@hotmail.com>
2025-05-14 02:15:29 +0300

    Added tests in main.

--- rasterizeddb_core/src/main.rs
+++ rasterizeddb_core/src/main.rs
@@ -1,10 +1,17 @@
+use std::sync::atomic::AtomicU64;
 use std::sync::Arc;
 use std::arch::x86_64::{_mm_prefetch, _MM_HINT_T0};
 
 use log::LevelFilter;
 use rasterizeddb_core::core::database::Database;
 
+use rasterizeddb_core::core::db_type::DbType;
+use rasterizeddb_core::core::row_v2::row::{ColumnFetchingData, ColumnWritePayload, RowFetch, RowWrite};
+use rasterizeddb_core::core::row_v2::row_pointer::{RowPointer, RowPointerIterator};
+use rasterizeddb_core::core::storage_providers::mock_file_sync::MockStorageProvider;
+use rasterizeddb_core::core::storage_providers::traits::StorageIO;
 use rasterizeddb_core::core::table::Table;
+use rasterizeddb_core::memory_pool::MEMORY_POOL;
 use rasterizeddb_core::rql::parser::{parse_rql, ParserResult};
 use rasterizeddb_core::{
     core::storage_providers::file_sync::LocalStorageProvider,
@@ -16,6 +23,110 @@ use tokio::fs::remove_file;
 #[tokio::main(flavor = "multi_thread")]
 #[allow(unreachable_code)]
 async fn main() -> std::io::Result<()> {
+    let mut mock_io_pointers = MockStorageProvider::new().await;
+    let mut mock_io_rows = MockStorageProvider::new().await;
+
+    println!("{}", mock_io_pointers.get_location().unwrap());
+    println!("{}", mock_io_rows.get_location().unwrap());
+
+    #[cfg(feature = "enable_long_row")]
+    let cluster = 0;
+
+    let last_id = AtomicU64::new(0);
+    let table_length = AtomicU64::new(0);
+
+    let string_bytes = b"Hello, world!";
+    let string_data = MEMORY_POOL.acquire(string_bytes.len() + 4);
+    let mut string_data_wrapper = unsafe { string_data.into_wrapper() };
+    let string_vec = string_data_wrapper.as_vec_mut();
+    let string_size = string_bytes.len() as u32;
+    let string_size_bytes = string_size.to_le_bytes();
+    string_vec[0] = string_size_bytes[0];
+    string_vec[1] = string_size_bytes[1];
+    string_vec[2] = string_size_bytes[2];
+    string_vec[3] = string_size_bytes[3];
+    string_vec[4..].copy_from_slice(string_bytes);
+
+    let i32_bytes = 42_i32.to_le_bytes();
+    let i32_data = MEMORY_POOL.acquire(i32_bytes.len());
+    let mut i32_data_wrapper = unsafe { i32_data.into_wrapper() };
+    let i32_vec = i32_data_wrapper.as_vec_mut();
+    i32_vec.copy_from_slice(&i32_bytes);
+
+    let row_write = RowWrite {
+        columns_writing_data: vec![
+            ColumnWritePayload {
+                data: i32_data,
+                write_order: 0,
+                column_type: DbType::I32,
+                size: i32_bytes.len() as u32
+            },            
+            ColumnWritePayload {
+                data: string_data,
+                write_order: 1,
+                column_type: DbType::STRING,
+                size: string_bytes.len() as u32 + 4
+            },
+        ],
+    };
+
+    let result = RowPointer::write_row(
+        &mut mock_io_pointers, 
+        &mut mock_io_rows, 
+        &last_id, 
+        &table_length, 
+        #[cfg(feature = "enable_long_row")]
+        cluster, 
+        &row_write
+    ).await;
+
+    assert!(result.is_ok());
+
+    let mut iterator = RowPointerIterator::new(&mut mock_io_pointers).await.unwrap();
+
+    let next_pointer = iterator.next_row_pointer().await.unwrap().unwrap();
+
+    println!("Row pointer: {:?}", next_pointer);
+
+    let row_fetch = RowFetch {
+        columns_fetching_data: vec![
+            ColumnFetchingData {
+                column_offset: 0,
+                column_type: DbType::I32,
+                size: i32_bytes.len() as u32
+            },
+            ColumnFetchingData {
+                column_offset: 4,
+                column_type: DbType::STRING,
+                size: string_bytes.len() as u32 + 4
+            }
+        ],
+    };
+
+    let row = next_pointer.fetch_row(
+        &mut mock_io_rows, 
+        &row_fetch
+    ).await.unwrap();
+
+    row.columns.iter().for_each(|column| {
+        match column.column_type {
+            DbType::STRING => {
+                let wrapper = unsafe { column.data.into_wrapper() };
+                let string_data = wrapper.as_slice();
+                assert_eq!(string_data[0..4], string_size_bytes);
+                assert_eq!(&string_data[4..], string_bytes);
+            },
+            DbType::I32 => {
+                let wrapper = unsafe { column.data.into_wrapper() };
+                let i32_data = wrapper.as_slice();
+                assert_eq!(i32_data, i32_bytes);
+            },
+            _ => {}
+        }
+    });
+
+    return Ok(());
+
     env_logger::Builder::new()
         .filter_level(LevelFilter::Error)
         .init();
