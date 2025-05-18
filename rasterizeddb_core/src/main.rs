de65dcd2b1e33881c641b6cabdee08fb69496038
Milen Denev <mspc6d25@hotmail.com>
2025-05-18 14:10:17 +0300

    Updated tests in main.

--- rasterizeddb_core/src/main.rs
+++ rasterizeddb_core/src/main.rs
@@ -1,17 +1,18 @@
-use std::sync::atomic::AtomicU64;
 use std::sync::Arc;
 use std::arch::x86_64::{_mm_prefetch, _MM_HINT_T0};
 
+use futures::future::join_all;
+use itertools::Either;
 use log::LevelFilter;
 use rasterizeddb_core::core::database::Database;
 
 use rasterizeddb_core::core::db_type::DbType;
-use rasterizeddb_core::core::row_v2::row::{ColumnFetchingData, ColumnWritePayload, RowFetch, RowWrite};
-use rasterizeddb_core::core::row_v2::row_pointer::{RowPointer, RowPointerIterator};
-use rasterizeddb_core::core::storage_providers::mock_file_sync::MockStorageProvider;
-use rasterizeddb_core::core::storage_providers::traits::StorageIO;
+use rasterizeddb_core::core::mock_helpers::{get_row_fetch_i32, i32_column};
+use rasterizeddb_core::core::row_v2::row::Row;
+use rasterizeddb_core::core::row_v2::row_pointer::RowPointerIterator;
+use rasterizeddb_core::core::row_v2::transformer::{ColumnTransformer, ColumnTransformerType, ComparerOperation};
+
 use rasterizeddb_core::core::table::Table;
-use rasterizeddb_core::memory_pool::MEMORY_POOL;
 use rasterizeddb_core::rql::parser::{parse_rql, ParserResult};
 use rasterizeddb_core::{
     core::storage_providers::file_sync::LocalStorageProvider,
@@ -20,183 +21,163 @@ use rasterizeddb_core::{
 
 use stopwatch::Stopwatch;
 use tokio::fs::remove_file;
+use tokio::sync::{mpsc, Semaphore};
+use tokio::task;
+
+// CREATE Arc<(TUPLE)>
+// TO REDUCE REFERENCE COUNTING
 
 #[tokio::main(flavor = "multi_thread")]
 #[allow(unreachable_code)]
 async fn main() -> std::io::Result<()> {
-    let mut mock_io_pointers = MockStorageProvider::new().await;
-    let mut mock_io_rows = MockStorageProvider::new().await;
+    let stdin = std::io::stdin();
+    let mut buffer = String::new();
+    println!("Press Enter to start...");
+    stdin.read_line(&mut buffer).unwrap();
 
-    println!("{}", mock_io_pointers.get_location().unwrap());
-    println!("{}", mock_io_rows.get_location().unwrap());
+    const SEARCH_VALUE: i32 = 1000;
+
+    //let mut mock_io_pointers = MockStorageProvider::new().await;
+    //let mut mock_io_rows = MockStorageProvider::new().await;
+
+    let mut mock_io_pointers = LocalStorageProvider::new("G:\\Databases\\Test_Database", Some("pointers.db")).await;
+    let mock_io_rows = LocalStorageProvider::new("G:\\Databases\\Test_Database", Some("rows.db")).await;
+
+    // println!("{}", mock_io_pointers.get_location().unwrap());
+    // println!("{}", mock_io_rows.get_location().unwrap());
 
     #[cfg(feature = "enable_long_row")]
     let cluster = 0;
 
-    let last_id = AtomicU64::new(0);
-    let table_length = AtomicU64::new(0);
+    // let last_id = AtomicU64::new(1_000_000);
+    // let table_length = AtomicU64::new(mock_io_rows.get_len().await);
 
-    // Column 0
-    let string_bytes = b"Hello, world!";
+    //let row_write = create_row_write(true);
+    //let custom_row = create_row_write_custom_i32(SEARCH_VALUE, true);
 
-    let string_data = MEMORY_POOL.acquire(string_bytes.len());
-    let mut string_data_wrapper = unsafe { string_data.into_wrapper() };
-    let string_vec = string_data_wrapper.as_vec_mut();
+    // for _i in 0..10_000_000 {
+    //     _ = RowPointer::write_row(
+    //         &mut mock_io_pointers, 
+    //         &mut mock_io_rows, 
+    //         &last_id, 
+    //         &table_length, 
+    //         #[cfg(feature = "enable_long_row")]
+    //         cluster, 
+    //         &row_write
+    //     ).await;
+    // }
 
-    string_vec[0..].copy_from_slice(string_bytes);
+    // let result = RowPointer::write_row(
+    //     &mut mock_io_pointers, 
+    //     &mut mock_io_rows, 
+    //     &last_id, 
+    //     &table_length, 
+    //     #[cfg(feature = "enable_long_row")]
+    //     cluster, 
+    //     &custom_row
+    // ).await;
 
-    // Column 1
-    
-    let large_string = "Hello, world!".repeat(50);
-    let large_string_bytes = large_string.as_bytes();
-
-    let large_string_data = MEMORY_POOL.acquire(large_string_bytes.len());
-    let mut large_string_data_wrapper = unsafe { large_string_data.into_wrapper() };
-    let large_string_vec = large_string_data_wrapper.as_vec_mut();
-
-    large_string_vec[0..].copy_from_slice(large_string_bytes);
-
-    // Column 2
-
-    let i32_bytes = 42_i32.to_le_bytes();
-    let i32_data = MEMORY_POOL.acquire(i32_bytes.len());
-    let mut i32_data_wrapper = unsafe { i32_data.into_wrapper() };
-    let i32_vec = i32_data_wrapper.as_vec_mut();
-    i32_vec.copy_from_slice(&i32_bytes);
-
-    let row_write = RowWrite {
-        columns_writing_data: vec![
-            ColumnWritePayload {
-                data: string_data,
-                write_order: 0,
-                column_type: DbType::STRING,
-                size: 4 + 8 // String Size + String Pointer
-            },
-            ColumnWritePayload {
-                data: i32_data,
-                write_order: 1,
-                column_type: DbType::I32,
-                size: i32_bytes.len() as u32
-            },
-            ColumnWritePayload {
-                data: large_string_data,
-                write_order: 2,
-                column_type: DbType::STRING,
-                size: 4 + 8 // String Size + String Pointer
-            },
-        ],
-    };
-
-    _ = RowPointer::write_row(
-        &mut mock_io_pointers, 
-        &mut mock_io_rows, 
-        &last_id, 
-        &table_length, 
-        #[cfg(feature = "enable_long_row")]
-        cluster, 
-        &row_write
-    ).await;
-
-    _ = RowPointer::write_row(
-        &mut mock_io_pointers, 
-        &mut mock_io_rows, 
-        &last_id, 
-        &table_length, 
-        #[cfg(feature = "enable_long_row")]
-        cluster, 
-        &row_write
-    ).await;
-
-    _ = RowPointer::write_row(
-        &mut mock_io_pointers, 
-        &mut mock_io_rows, 
-        &last_id, 
-        &table_length, 
-        #[cfg(feature = "enable_long_row")]
-        cluster, 
-        &row_write
-    ).await;
-
-    _ = RowPointer::write_row(
-        &mut mock_io_pointers, 
-        &mut mock_io_rows, 
-        &last_id, 
-        &table_length, 
-        #[cfg(feature = "enable_long_row")]
-        cluster, 
-        &row_write
-    ).await;
-
-    let result = RowPointer::write_row(
-        &mut mock_io_pointers, 
-        &mut mock_io_rows, 
-        &last_id, 
-        &table_length, 
-        #[cfg(feature = "enable_long_row")]
-        cluster, 
-        &row_write
-    ).await;
-
-    println!("Row write result: {:?}", result);
-
-    assert!(result.is_ok());
+    // if let Err(e) = result {
+    //     println!("Error writing row: {}", e);
+    // }
 
     let mut iterator = RowPointerIterator::new(&mut mock_io_pointers).await.unwrap();
-
     let mut stopwatch = Stopwatch::start_new();
+    let row_fetch = get_row_fetch_i32();
+    let search_value = SEARCH_VALUE;
+    let search_value_block = i32_column(search_value);
+    let (tx, mut rx) = mpsc::unbounded_channel::<Row>();
 
-    let next_pointer = iterator.next_row_pointer().await.unwrap().unwrap();
+    let arc_tuple = Arc::new((Semaphore::new(16), mock_io_rows, row_fetch));
 
-    let row_fetch = RowFetch {
-        columns_fetching_data: vec![
-            ColumnFetchingData {
-                column_offset: 0,
-                column_type: DbType::STRING,
-                size: 0 // String size is not known yet
-            },
-            ColumnFetchingData {
-                column_offset: 8 + 4,
-                column_type: DbType::I32,
-                size: i32_bytes.len() as u32
-            },
-            // ColumnFetchingData {
-            //     column_offset: 8 + 4 + 4,
-            //     column_type: DbType::STRING,
-            //     size: 0 // String size is not known yet
-            // },
-        ],
-    };
+    // Collect handles for all batch tasks
+    let mut batch_handles = Vec::new();
 
-    let row = next_pointer.fetch_row(
-        &mut mock_io_rows, 
-        &row_fetch
-    ).await.unwrap();
+    // Process batches
+    while let Ok(pointers) = iterator.next_row_pointers().await {
+        if pointers.is_empty() {
+            break;
+        }
+
+        let tuple_clone = arc_tuple.clone();
+
+        let tx_clone = tx.clone();
+        let search_value_block_clone = search_value_block.clone();
+        //let i_clone = i.clone();
+
+        // Spawn a task for this entire batch of pointers
+        let batch_handle = task::spawn(async move {
+            // Acquire a permit for this batch
+            let batch_permit = tuple_clone.0.acquire().await.unwrap();
+
+            let mut row = Row::default();
+
+            // Process each pointer in this batch (same as your original code)
+            for pointer in pointers {
+                let tuple_clone_2 = tuple_clone.clone();
+                let (_, io_clone, row_fetch) = &*tuple_clone_2;
+
+                let search_value_block_clone_2 = search_value_block_clone.clone();
+             
+                pointer.fetch_row_reuse_async(io_clone, row_fetch, &mut row).await;
+                
+                let transformer = ColumnTransformer::new(
+                    DbType::I32,
+                    search_value_block_clone_2,
+                    row.columns[0].data.clone(),
+                    ColumnTransformerType::ComparerOperation(ComparerOperation::Equals),
+                    None
+                );
+                
+                let result = transformer.transform_single();
+                
+                if let Either::Right(found) = result {
+                    if found {
+                        println!("Found row with ID: {}", pointer.id);
+                        tx_clone.send(Row::clone_from_mut_row(&row)).unwrap();
+                    }
+                }
+
+            }
+
+            // Release the batch semaphore permit
+            drop(batch_permit);
+        });
+        
+        batch_handles.push(batch_handle);
+    }
+
+    // Wait for all batch tasks to complete
+    join_all(batch_handles).await;
+
+    // Drop the sender to signal that no more rows will be sent
+    drop(tx);
+
+    // Aggregator remains the same
+    let aggregator_handle = tokio::spawn(async move {
+        let mut collected_rows = Vec::with_capacity(50);
+        
+        if !rx.is_empty() {
+            rx.recv_many(&mut collected_rows, usize::MAX).await;
+        }
+        
+        drop(rx);
+        collected_rows
+    });
+
+    // Await the aggregator to finish collecting rows.
+    let all_rows = aggregator_handle.await.unwrap();
+
+    println!("Total rows collected: {}", all_rows.len());
 
     stopwatch.stop();
 
     println!("Row fetch took: {:?}", stopwatch.elapsed());
     
-    row.columns.iter().for_each(|column| {
-        match column.column_type {
-            DbType::STRING => {
-                println!("STRING column: {:?}", column.data);
-                let wrapper = unsafe { column.data.into_wrapper() };
-                let string_data = wrapper.as_vec();
-                if string_data.len() < 100 {
-                    assert_eq!(string_data, string_bytes);
-                } else {
-                    assert_eq!(string_data, large_string_bytes);
-                } 
-            },
-            DbType::I32 => {
-                println!("I32 column: {:?}", column.data);
-                let wrapper = unsafe { column.data.into_wrapper() };
-                let i32_data = wrapper.as_slice();
-                assert_eq!(i32_data, i32_bytes);
-            },
-            _ => {}
-        }
-    });
+    let stdin = std::io::stdin();
+    let mut buffer = String::new();
+    println!("Press Enter to continue...");
+    stdin.read_line(&mut buffer).unwrap();
 
     return Ok(());
 
