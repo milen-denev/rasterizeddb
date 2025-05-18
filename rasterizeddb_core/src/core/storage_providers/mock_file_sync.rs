4dc02d89646af6047f87f5b5aade0151cf3f7dc9
Milen Denev <mspc6d25@hotmail.com>
2025-05-19 00:39:27 +0300

    Update mock io.

--- rasterizeddb_core/src/core/storage_providers/mock_file_sync.rs
+++ rasterizeddb_core/src/core/storage_providers/mock_file_sync.rs
@@ -4,10 +4,15 @@ use std::{
     path::{Path, PathBuf}, sync::Arc
 };
 
+use crossbeam_queue::SegQueue;
+use futures::future::join_all;
+use memmap2::{Mmap, MmapOptions};
 use temp_testdir::TempDir;
-use tokio::{io::{AsyncSeekExt, AsyncWriteExt}, sync::RwLock, task::yield_now};
+use tokio::{io::{AsyncReadExt, AsyncSeekExt, AsyncWriteExt}, sync::RwLock, task::yield_now};
 
-use super::traits::StorageIO;
+use crate::memory_pool::MemoryBlock;
+
+use super::{traits::StorageIO, CRC};
 
 pub struct MockStorageProvider {
     pub(super) append_file: Arc<RwLock<tokio::fs::File>>,
@@ -15,7 +20,10 @@ pub struct MockStorageProvider {
     pub(crate) location: String,
     pub(crate) table_name: String,
     pub(crate) file_str: String,
-    _temp_dir_hold: TempDir
+    _temp_dir_hold: TempDir,
+    pub(crate) hash: u32,
+    appender: SegQueue<MemoryBlock>,
+    pub(crate) memory_map: Mmap,
 }
 
 unsafe impl Sync for MockStorageProvider { }
@@ -29,7 +37,12 @@ impl Clone for MockStorageProvider {
             location: self.location.clone(),
             table_name: self.table_name.clone(),
             file_str: self.file_str.clone(),
-            _temp_dir_hold: TempDir::default()
+            _temp_dir_hold: TempDir::default(),
+            hash: CRC.checksum(format!("{}+++{}", self.location, "temp.db").as_bytes()),
+            appender: SegQueue::new(),
+            memory_map: unsafe { MmapOptions::new()
+                .map(&std::fs::File::open(&self.file_str).unwrap())
+                .unwrap() },
         }
     }
 }
@@ -73,16 +86,25 @@ impl MockStorageProvider {
             .await
             .unwrap();
 
+            
+        let file_read = std::fs::File::options()
+            .read(true)
+            .open(&file_str)
+            .unwrap();
+
         MockStorageProvider {
             append_file: Arc::new(RwLock::new(file_append)),
             write_file: Arc::new(RwLock::new(file_write)),
             location: file_path.to_str().unwrap().to_string(),
             table_name: "temp.db".to_string(),
             file_str: file_str,
-            _temp_dir_hold: temp
+            _temp_dir_hold: temp,
+            hash: CRC.checksum(format!("{}+++{}", file_path.to_str().unwrap(), "temp.db").as_bytes()),
+            appender: SegQueue::new(),
+            memory_map: unsafe { MmapOptions::new()
+                    .map(&file_read)
+                    .unwrap() },
         }
-    
-
     }
 
     pub async fn close_files(&mut self) {
@@ -106,6 +128,26 @@ impl MockStorageProvider {
         _ = file_read.sync_all();
         _ = file_append.sync_all();
         _ = file_write.sync_all();
+    }
+
+    pub async fn start_append_data_service(&mut self) {
+        loop {
+            if self.appender.len() > 0 {
+                let mut buffer: Vec<u8> = Vec::default();
+
+                while let Some(block) = self.appender.pop() {
+                    buffer.extend_from_slice(block.into_slice());
+    
+                }
+
+                let mut file = self.append_file.write().await;
+                file.write_all(&buffer).await.unwrap();
+                file.flush().await.unwrap();
+                file.sync_all().await.unwrap();
+            } else {
+                tokio::time::sleep(tokio::time::Duration::from_millis(10)).await;
+            }
+        }
     }
 }
 
@@ -165,28 +207,26 @@ impl StorageIO for MockStorageProvider {
     }
 
     async fn read_data_to_end(&self, position: u64) -> Vec<u8> {
-        yield_now().await;
-        
-        let mut read_file = std::fs::File::options()
+        let mut read_file = tokio::fs::File::options()
             .read(true)
             .open(&self.file_str)
+            .await
             .unwrap();
 
-        read_file.seek(SeekFrom::Start(position)).unwrap();
+        read_file.seek(SeekFrom::Start(position)).await.unwrap();
         let mut buffer: Vec<u8> = Vec::default();
-        read_file.read_to_end(&mut buffer).unwrap();
+        read_file.read_to_end(&mut buffer).await.unwrap();
         return buffer;
     }
 
     async fn get_len(&mut self) -> u64 {
-        yield_now().await;
-
-        let read_file = std::fs::File::options()
+        let read_file = tokio::fs::File::options()
             .read(true)
             .open(&self.file_str)
+            .await
             .unwrap();
         
-        let len = read_file.metadata().unwrap().len();
+        let len = read_file.metadata().await.unwrap().len();
         len
     }
 
@@ -210,17 +250,77 @@ impl StorageIO for MockStorageProvider {
         }
     }
 
+    #[allow(unreachable_code)]
     async fn read_data_into_buffer(&self, position: &mut u64, buffer: &mut [u8]) {
-        yield_now().await;
-        
-        let mut read_file = std::fs::File::options()
-            .read(true)
-            .open(&self.file_str)
-            .unwrap();
+        // use crate::{core::row_v2::row_pointer::RowPointer, memory_pool::{MemoryBlock, MEMORY_POOL}};
+        // fn get_range(position: &u64, max: u64) -> (u64,u64) {
+        //     const BLOCK_SIZE: u64 = 1024 * 1024 * 16; // 16MB
+        //     let chunk = position / BLOCK_SIZE;
+        //     let start = chunk * BLOCK_SIZE;
+        //     let mut end = (chunk + 1) * BLOCK_SIZE;
 
-        read_file.seek(SeekFrom::Start(*position)).unwrap();
-        read_file.read(buffer).unwrap();
-        *position += buffer.len() as u64;
+        //     if end > max {
+        //         end = max;
+        //     }
+
+        //     (start, end)
+        // }
+
+        // let range = get_range(position, self.file_len.load(std::sync::atomic::Ordering::Relaxed));
+
+        // if let Some(block) = CACHE_MAP.pin().get(&(range.1, self.hash)) {
+        //     let slice = block.into_slice();
+        //     let new_position = *position - range.0;
+        //     let buffer_len = buffer.len();
+
+        //     if slice.len() > new_position as usize + buffer_len {
+        //         // Copy the available data from the cached block
+        //         buffer.copy_from_slice(&slice[new_position as usize..new_position as usize + buffer_len]);
+        //         *position += buffer_len as u64;
+        //         return;
+        //     } else {
+        //         // This is the case where we need to handle partial read from cache and rest from memory map
+        //         let available_len = slice.len() - new_position as usize; // How much we can read from cache
+                
+        //         // Copy the available part from cache
+        //         buffer[..available_len].copy_from_slice(&slice[new_position as usize..]);
+                
+        //         // Calculate the remaining part position and size
+        //         let remaining_size = buffer_len - available_len;
+        //         let remaining_position = range.0 + new_position as u64 + available_len as u64;
+                
+        //         // Copy the remaining part from memory map
+        //         buffer[available_len..].copy_from_slice(
+        //             &self.memory_map[remaining_position as usize..remaining_position as usize + remaining_size]
+        //         );
+                
+        //         *position += buffer_len as u64;
+        //         return;
+        //     }
+        // } else {
+        //     let range = (range.0, range.1, RowPointer::get_timestamp_seconds());
+        //     let block = MEMORY_POOL.acquire((range.1 - range.0) as usize);
+        //     let slice = block.into_slice_mut();
+
+        //     slice.copy_from_slice(&self.memory_map[range.0 as usize..range.1 as usize]);
+
+        //     CACHE_MAP.pin_owned().insert((range.1, self.hash), block);
+
+        //     let buffer_len = buffer.len() as u64;
+
+        //     *position += buffer_len;
+
+        //     buffer.copy_from_slice(&slice[(*position - range.0) as usize..*position as usize - range.0 as usize + buffer_len as usize]);
+        // }
+
+        // return;
+    
+        let mmap = &self.memory_map;
+        let buffer_len = buffer.len();
+
+        buffer.copy_from_slice(&mmap[*position as usize..*position as usize + buffer_len]);
+
+        *position += buffer_len as u64;
     }
 
     async fn read_data_to_cursor(&self, position: &mut u64, length: u32) -> Cursor<Vec<u8>> {
@@ -312,13 +412,23 @@ impl StorageIO for MockStorageProvider {
             .await
             .unwrap();
 
+        let file_read = std::fs::File::options()
+            .read(true)
+            .open(&file_str)
+            .unwrap();
+
         Self {
             append_file: Arc::new(RwLock::new(file_append)),
             write_file: Arc::new(RwLock::new(file_write)),
             location: self.location.to_string(),
             table_name: "temp.db".to_string(),
             file_str: file_str,
-            _temp_dir_hold: TempDir::default()
+            _temp_dir_hold: TempDir::default(),
+            hash: CRC.checksum(format!("{}+++{}", self.location, "temp.db").as_bytes()),
+            appender: SegQueue::new(),
+            memory_map: unsafe { MmapOptions::new()
+                .map(&file_read)
+                .unwrap() },
         }
     }
 
@@ -376,13 +486,23 @@ impl StorageIO for MockStorageProvider {
             .await
             .unwrap();
 
+        let file_read = std::fs::File::options()
+                .read(true)
+                .open(&file_path)
+                .unwrap();
+
         MockStorageProvider {
             append_file: Arc::new(RwLock::new(file_append)),
             write_file: Arc::new(RwLock::new(file_write)),
             location: self.location.to_string(),
             table_name: name.to_string(),
             file_str: new_table.clone(),
-            _temp_dir_hold: TempDir::default()
+            _temp_dir_hold: TempDir::default(),
+            hash: CRC.checksum(format!("{}+++{}", file_path.to_str().unwrap(), "temp.db").as_bytes()),
+            appender: SegQueue::new(),
+            memory_map: unsafe { MmapOptions::new()
+                .map(&file_read)
+                .unwrap() },
         }
     }
 
@@ -401,6 +521,18 @@ impl StorageIO for MockStorageProvider {
 
         remove_file(&path).unwrap();
     }
+    
+    fn get_hash(&self) -> u32 {
+        self.hash
+    }
+
+    async fn start_service(&mut self) {
+        let services = vec![
+            self.start_append_data_service()
+        ];
+
+        join_all(services).await;
+    } 
 }
 
 impl Drop for MockStorageProvider {
