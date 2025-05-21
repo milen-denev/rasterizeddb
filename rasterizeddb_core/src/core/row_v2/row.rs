717c54024ef5cc81d00662678ad1e026e4894cdb
Milen Denev <mspc6d25@hotmail.com>
2025-05-21 19:16:42 +0300

    new helper api for columns.

--- rasterizeddb_core/src/core/row_v2/row.rs
+++ rasterizeddb_core/src/core/row_v2/row.rs
@@ -1,4 +1,8 @@
+use dashmap::DashMap;
+
 use crate::{core::db_type::DbType, memory_pool::MemoryBlock};
+
+use super::schema::SchemaField;
 
 pub struct RowFetch {
     pub columns_fetching_data: Vec<ColumnFetchingData>
@@ -48,4 +52,29 @@ pub struct Column {
     pub schema_id: u64,
     pub data: MemoryBlock,
     pub column_type: DbType,
+}
+
+pub fn column_vec_into_dashmap(
+    columns: &Vec<Column>,
+    schema: &Vec<SchemaField>
+) -> DashMap<String, MemoryBlock> {
+    let column_map = DashMap::new();
+
+    for column in columns {
+        column_map.insert(schema[column.schema_id as usize].name.clone(), column.data.clone());
+    }
+
+    column_map
+}
+
+pub fn update_values_of_dashmap(
+    column_map: &DashMap<String, MemoryBlock>,
+    columns: &Vec<Column>,
+    schema: &Vec<SchemaField>
+) {
+    for column in columns {
+        column_map.alter(&schema[column.schema_id as usize].name, |_, _| {
+            column.data.clone()
+        });
+    }
 }
