6ed4ee9e702e181d36bc8935c09e4029a75691a8
Milen Denev <mspc6d25@hotmail.com>
2025-07-31 15:25:21 +0300

    changes to testing.

--- rasterizeddb_core/src/main.rs
+++ rasterizeddb_core/src/main.rs
@@ -19,8 +19,8 @@ async fn main() -> std::io::Result<()> {
     tokio::spawn(io_rows.start_service());
     tokio::spawn(io_pointers.start_service());
 
-    // rasterizeddb_core::core::mock_table::
-    //     consolidated_write_data_function(100_000).await;
+    rasterizeddb_core::core::mock_table::
+        consolidated_write_data_function(999).await;
 
     let schema = get_schema().await;
 
@@ -29,7 +29,7 @@ async fn main() -> std::io::Result<()> {
     println!("Press Enter to start...");
     stdin.read_line(&mut buffer).unwrap();
 
-    consolidated_read_data_function(schema, 99_999).await;
+    consolidated_read_data_function(schema, 998).await;
 
     let stdin = std::io::stdin();
     let mut buffer = String::new();
