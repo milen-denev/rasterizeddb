d26e734b3b3f33e023893e296453809f07c8c3c9
Milen Denev <mspc6d25@hotmail.com>
2025-08-23 15:18:33 +0300

    remove comments from past testing functions from main.

--- rasterizeddb_core/src/main.rs
+++ rasterizeddb_core/src/main.rs
@@ -7,7 +7,7 @@ use tokio::runtime::Builder;
 #[allow(unreachable_code)]
 #[allow(static_mut_refs)]
 fn main() -> std::io::Result<()> {
-        // 64 MiB
+    // 64 MiB
     let stack_size = 64 * 1024 * 1024;
 
     let rt = Builder::new_multi_thread()
@@ -16,42 +16,13 @@ fn main() -> std::io::Result<()> {
         .enable_all()
         .build()?;
 
-    // let io_rows = unsafe { IO_ROWS.force_mut().await };
-    // let io_pointers = unsafe { IO_POINTERS.force_mut().await };
-    // let io_schema = unsafe { IO_SCHEMA.force_mut().await };
-
-    // tokio::spawn(io_rows.start_service());
-    // tokio::spawn(io_pointers.start_service());
-    // tokio::spawn(io_schema.start_service());
-
-    // rasterizeddb_core::core::mock_table::
-    //     consolidated_write_data_function(5_000_000 * 2).await;
-
-    // let stdin = std::io::stdin();
-    // let mut buffer = String::new();
-    // println!("Press Enter to start...");
-    // stdin.read_line(&mut buffer).unwrap();
-
-    // for i in 0..100 {
-    //     println!("Iteration: {}", i);
-    //     let schema = get_schema().await;
-    //     let _ = consolidated_read_data_function(schema, 599_999_999).await;
-    // }
-    
-    // let stdin = std::io::stdin();
-    // let mut buffer = String::new();
-    // println!("Press Enter to terminate...");
-    // stdin.read_line(&mut buffer).unwrap();
-
-    // return Ok(());
-
      rt.block_on(async {
         env_logger::Builder::new()
             .filter_level(LevelFilter::Error)
             .init();
 
             
-        let database = Database::new("C:\\Databases\\Production").await;
+        let database = Database::new("G:\\Databases\\Production").await;
         let arc_database = Arc::new(database);
         _ = Database::start_db(arc_database).await;
     });
