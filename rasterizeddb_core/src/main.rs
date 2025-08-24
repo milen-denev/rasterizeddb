f11b4162dbaf37ce0477f519fc247144ab39d797
Milen Denev <mspc6d25@hotmail.com>
2025-08-24 17:56:42 +0300

    update stack sizes.

--- rasterizeddb_core/src/main.rs
+++ rasterizeddb_core/src/main.rs
@@ -1,17 +1,17 @@
 use std::sync::Arc;
 
 use log::LevelFilter;
-use rasterizeddb_core::core::database_v2::Database;
+use rasterizeddb_core::{core::database_v2::Database, MAX_PERMITS_THREADS};
 use tokio::runtime::Builder;
 
 #[allow(unreachable_code)]
 #[allow(static_mut_refs)]
 fn main() -> std::io::Result<()> {
-    // 64 MiB
-    let stack_size = 64 * 1024 * 1024;
+    // 32 MiB
+    let stack_size = 32 * 1024 * 1024;
 
     let rt = Builder::new_multi_thread()
-        .worker_threads(16)
+        .worker_threads(MAX_PERMITS_THREADS)
         .thread_stack_size(stack_size)
         .enable_all()
         .build()?;
@@ -20,7 +20,6 @@ fn main() -> std::io::Result<()> {
         env_logger::Builder::new()
             .filter_level(LevelFilter::Error)
             .init();
-
             
         let database = Database::new("G:\\Databases\\Production").await;
         let arc_database = Arc::new(database);
