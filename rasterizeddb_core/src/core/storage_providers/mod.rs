4151a8c0a4724fbc1d41a38b6a537d3656ba16fc
Milen Denev <mspc6d25@hotmail.com>
2026-01-09 21:15:19 +0200

    fix unix wrong import.

--- rasterizeddb_core/src/core/storage_providers/mod.rs
+++ rasterizeddb_core/src/core/storage_providers/mod.rs
@@ -5,9 +5,6 @@ pub mod traits;
 pub mod mock_file_sync;
 pub mod helpers;
 
-#[cfg(unix)]
-pub mod io_uring_reader;
-
 use crc::{CRC_32_ISO_HDLC, Crc};
 
 pub const CRC: Crc<u32> = Crc::<u32>::new(&CRC_32_ISO_HDLC);
